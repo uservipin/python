2fb307905a2471fad652bfad1f120d1c9f37998c
vipin kumar <kumar.vipin1v00@gmail.com>
2021-08-24 18:47:00 +0530

    update python files

--- Pandas/load_csv_file.py
+++
@@ -1,8 +0,0 @@
-import  pandas as pd
-
-df= pd.read_csv("AAC.csv")
-
-#print(df)
-# to string is used to print compplet data frame
-
-print(df.to_string())
--- Seaborn/visualization.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import pyplot as plt
-import seaborn as sns
-df = sns.load_dataset('car_crashes')
-plt.scatter(df.speeding,df.alcohol)
-sns.set_style("ticks")
-sns.despine()
-plt.show()
-`
